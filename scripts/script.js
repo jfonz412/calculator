66a8e06da9052b7ad484971ee19d7c74cf871757
jfonz412 <jfonzone@outlook.com>
2017-08-18 20:11:15 -0400

    set up divs

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+});
+
+//Not currently used
+function drawButtons(){
+	for(var i=0;i<4;i++){
+		$('.calculator').append('<div class="row"></div>');
+	}
+	for(var i=0;i<4;i++){
+		$('.row').append('<div class="button"></div>');
+	}
+}
